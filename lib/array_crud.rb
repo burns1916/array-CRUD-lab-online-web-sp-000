094d8979cb7773887de4ad4edfb60e34286cf862
Ryan K Burns <ryankburns@gmail.com>
2020-09-18 03:58:24 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,7 +3,7 @@ def create_an_empty_array
 end
 
 def create_an_array(herbs)
-  herbs = [oregano, parsley, cilantro, mint]
+  [oregano, parsley, cilantro, mint]
 end
 
 def add_element_to_end_of_array(array, element)
